629cf9a4f1ee8338f00e300042dfb94ad864ed03
gangaratnam <gangaratnam.putti@artifixel.com>
2025-07-28 10:21:11 +0530

    routing

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,15 +1,17 @@
 import { Avatar } from "@mantine/core";
 import type React from "react"
 import { BsFillKanbanFill } from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
 
 
 const Header: React.FC = () => {
+    const navigate =useNavigate()
     return (
         <div className="header p-3 d-flex flex-between align-items-center shadow-2xs">
-            <div>
-                <BsFillKanbanFill size={26} className="black-font" />
+            <div onClick={()=>navigate("/")}>
+                <BsFillKanbanFill size={26} className="black-font cursor-pointer" />
             </div>
-            <div>
+            <div >
                 {/* <CgProfile size={26} className="black-font" /> */}
                   <Avatar variant="filled" radius="sm" color="yellow" src="" className="cursor-pointer" />
             </div>
--- src/layout/Sidebar.tsx
+++ src/layout/Sidebar.tsx
@@ -1,20 +1,29 @@
 import type React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Sidebar: React.FC = () => {
-    const items = ["Dashboard", "Tasks", "Calendar", "Teams", "Settings"];
+    const navigate = useNavigate()
+    const items = [{ name: "Dashboard", path: "/dashboard" }, {
+        name: "Tasks", path: "/"
+    }, { name: "Calendar", path: "/calendar" }, { name: "Teams", path: "/teams" }, { name: "Settings", path: "/settings" }];
+
+
 
     return (
-        <div className="flex flex-col items-center bg-gray-300 gap-2 min-h-screen">
-            {items.map((item, index) => (
-                <div
-                    key={index}
-                    className="w-full px-3 py-2 rounded hover:bg-gray-200  cursor-pointer text-black"
-                >
-                    {item}
-                </div>
-            ))}
+        <div className="flex flex-col items-center bg-gray-300 gap-2 min-h-screen" >
+            {
+                items.map((item, index) => (
+                    <div
+                        key={index}
+                        className="w-full px-3 py-2 rounded hover:bg-gray-200  cursor-pointer text-black"
+                        onClick={() => navigate(item?.path)}
+                    >
+                        {item?.name}
+                    </div>
+                ))
+            }
 
-        </div>
+        </div >
     )
 }
 export default Sidebar;
